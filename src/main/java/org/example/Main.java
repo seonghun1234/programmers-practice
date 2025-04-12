f7b8286356bec06b06a408ffb836c57cb9e842c6
s3496h <s3496h@gmail.com>
2025-04-12 19:55:32 +0900

    첫 번ì모음 제거

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String result = sol.solution("bus");
+        System.out.println("결과: " + result);
+    }
+}
