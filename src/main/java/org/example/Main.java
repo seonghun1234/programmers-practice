8ae32928b382c1972a23520d5f1a44a2826a3310
s3496h <s3496h@gmail.com>
2025-04-14 19:49:11 +0900

    각도문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution("I love you~"));
+        System.out.println("Hello World!");
     }
 }
--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,14 +1,26 @@
 package org.example;
 
 public class Solution {
-    public int solution(String message) {
-        int answer = 0;
-        String[] message1 = message.split("");
-
-        for (int i = 0; i <= message1.length; i++) {
-            answer = i*2;
+    public int solution(int angle) {
+        if (0 < angle && angle < 90) {
+            return 1;  // 예각
+        } else if (angle == 90) {
+            return 2;  // 직각
+        } else if (90 < angle && angle < 180) {
+            return 3;  // 둔각
+        } else {
+            return 4;  // 평각
         }
-        return answer;
     }
 
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] testAngles = {70,91,180};
+
+        for (int angle : testAngles) {
+            int result = sol.solution(angle);
+            System.out.println("angle = " + angle + ", result = " + result);
+        }
+    }
 }
